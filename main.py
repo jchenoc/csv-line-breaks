e32266b7a7af1596c805950d8d918fc9ba51d12b
jchenoc <jchen@openclinica.com>
2023-02-13 13:46:53 -0500

    update

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 def csv_to_list(filename, delim=','):
     filename = filename
     file_obj = open(filename, "r")
-    data = list(csv.reader(file_obj, delimiter=delim))
+    data = list(csv.reader(file_obj, delimiter=delim, quoting=csv.QUOTE_NONE))
     file_obj.close()
         
     return data
@@ -16,16 +16,21 @@ def fix_line_breaks(data):
     i = 0
     while i < len(data) - 1:
         while len(data[i]) < num_cols:
-            data[i] += data[i + 1]
-            data.pop(i + 1)
+            if len(data[i+1]) == 0:
+                data.pop(i+1)
+            else:
+                data[i][-1] += f" {data[i+1][0]}"
+                to_add = data.pop(i+1)
+                data[i] += to_add[1:]
         i += 1
     return data
 
 # checks if all rows match the necessary number of columns
-def check_line_breaks(filename, data):
+def check_line_breaks(filename, data, num_cols):
+    print(filename)
     for i in range(len(data)):
         if len(data[i]) != num_cols:
-            print(f"{filename} - row:{i}")
+            print(f"{filename} - row:{i}, numcols:{num_cols}, length:{len(data[i])}")
 
 # save python list as csv
 def save_to_csv(filename, data, delim=','):
@@ -42,5 +47,5 @@ if __name__ == "__main__":
         data = csv_to_list(f, delim=delim)
         num_cols = len(data[0])
         data_fixed = fix_line_breaks(data)
-        check_line_breaks(f,data_fixed)
+        check_line_breaks(f,data_fixed, num_cols)
         save_to_csv(f, data_fixed, delim=delim)
